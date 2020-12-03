c222e489ffee53c24070b62c2fabfbcf00a0d3da
dvaders97 <56842071+dvaders97@users.noreply.github.com>
2020-12-03 17:07:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>
+			HBD TINNIIII
+		</title>
+		<style>
+			.message{
+				background-image: url("img/bday_collage_final_2.png");
+				background-repeat: no-repeat;
+				background-attachment: fixed;
+				background-position: center;
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&family=Tangerine:wght@700&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container message">
+			<p class="birthday_heading">Happy Birthday Shohini</p>
+			<div class="main_body">
+			<p>Hi there!! Did someone just become 24 years old? Buri hoye jachhis toh!!(Please dont get angry :'3). Before everything, I want to wish you a Happy 21'st Birthday, just like we promised each other that no matter what our age be, we will be wishing each other this. Today is a special day for you, although getting old is a bit of a drag actually but let's just ignore everything and think of the good parts.</p>
+			<p><img class="bachha_buri" src="img/bachha_buri.jpg" width="320" height="240"></p>
+			<p><img class="dancer" src="img/dancer.jpg" width="320" height="240"></p>
+			<p>Anyway be it your Birthday or just a normal day, I cant stop appreciating you, your strengths as well as your flaws(Yes including you spiralling :P) and also can't ignore the fact that how gorgeous and beautiful you are. You look like a living manifestation of Ma Durga in this photo!</p>
+			<p>You are the most talented and artistic girl I have ever come across my entire life and you are also a piece of art crafted by God and your parents. I just cant stop admiring you. And please never think less of yourself, be proud of who you are and what you do. Come to think of it you are such an amazing dancer, although I was only able to watch it once live and I would certainly would like to see more of your dancing.</p>
+			<p><img class="seagirl" src="img/seagirl.jpg" width="320" height="240"></p>
+			<p>I am thankful to God that I met you because i learnt a lot of things from you mostly the fact how you stayed strong even after some of the most unfortunate events of your life. Be like this girl, keep your head high and shine throughout the future. If you ever fall down, rise up again from your ashes like a Phoneix and be the badass girl that you are. You deserve every bit of happiness in this world. And I also want you to know that never feel alone, this potato will always be by your side :D . I will always be there for you to cheer you up even in the darkest of times so that beautiful smile doesn't fade away. Hope you got your salary by now, so enjoy and gift yourself some good props, have some good food :D. Below is a small appreciation video I made for you. Hope you'll like it :') :')!! I know i should have asked permission before taking the photos from your Instagram profile, its your birthday so forgive me maybe please? </p>
+			</div>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe class="embed-responsive-item" src="video/compilation.mp4"></iframe>
+			</div>
+			<!--<div class="video_player">
+				<video controls>
+					<source src="video/compilation.mp4" type="video/mp4">
+				</video>
+			</div>-->
+		</div>
+	</body>
+</html>
